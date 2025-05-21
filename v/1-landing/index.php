2ef6ee9a82aa97f1c03ff471ab8cffcf07e764cd
Alex Piacentini <piacentini.alex@gmail.com>
2025-05-21 18:52:55 +0200

    landing ok and online

---
+++ v/1-landing/index.php
@@ -0,0 +1,127 @@
+<?php $vers = "?v=0.6"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+  <meta http-equiv="content-language" content="en">
+
+  <!-- Metadata -->
+
+  <title>Earth Rising | Messages from the Pale Blue Dot</title>
+  <meta name="description" content="⁠A concept album in which artists, poets, writers and musicians hold a mirror to humanity.">
+  <meta property="og:title" content="Earth Rising: Messages from the Pale Blue Dot">
+  <meta property="og:description" content="⁠A concept album in which artists, poets, writers and musicians hold a mirror to humanity.">
+  <meta property="og:image" content="https://earth-rising.com/assets/images/er-social-preview-v2.jpg" />
+  <meta name="twitter:card" content="summary_large_image">
+
+  <!-- Link -->
+
+  <link rel="stylesheet" href="./assets/css/index.css<?= $vers ?>" />
+  <link rel="stylesheet" href="./assets/css/bootstrap-custom.css<?= $vers ?>" />
+
+  <!-- Favicon -->
+
+  <link rel="icon" type="image/png" href="./assets/favicon/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="./assets/favicon/favicon.svg" />
+  <link rel="shortcut icon" href="./assets/favicon/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Earth Rising" />
+  <link rel="manifest" href="./assets/favicon/site.webmanifest" />
+
+  <!-- Consent banner -->
+
+  <script src="https://app.termly.io/resource-blocker/38020f00-d112-48ba-9a11-e09d66cf7a66?autoBlock=on"></script>
+
+  <!-- Google tag (gtag.js) -->
+
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-HFG0X77R6E"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-HFG0X77R6E');
+  </script>
+
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-WZDJS42M');
+  </script>
+  <!-- End Google Tag Manager -->
+</head>
+
+<body>
+
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZDJS42M"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <main>
+
+    <section class="block first">
+      <div>
+        <h1>EARTH RISING</h1>
+        <h2>Messages from the&nbsp;Pale&nbsp;Blue&nbsp;Dot</h2>
+      </div>
+    </section>
+
+    <hr />
+
+    <section class="block">
+      <div class="center-square" style="background-image: url('./assets/images/er-pattern-square.png<?= $vers ?>');">
+        <div class="circle">
+          <div>
+            <h2>V⁠OLUME I</h2>
+            <img class="my-2 pt-1" src="./assets/images/stars-volume-1.svg" alt="Earth Rising Volume I" />
+            <h2>Out 20.06.2025</h2>
+            <div class="only-dsk" role="space" style="height: 3em;"></div>
+            <p class="only-dsk">
+              Be first to hear the album.
+              <br /><a role="button" href="https://www.artangel.org.uk/about_us/newsletter/" target="_blank">
+                Subscribe to the Artangel<br />mailing list
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr />
+
+    <section class="block last">
+      <div>
+        <p class="only-mob mt-4">
+          Be first to hear the album.
+          <br /><a role="button" href="https://www.artangel.org.uk/about_us/newsletter/" target="_blank">
+            Subscribe to the Artangel mailing list
+          </a>
+        </p>
+        <div class="only-mob" role="space" style="height: 3em;"></div>
+        <p>⁠A concept album in which artists, poets, writers and musicians hold a mirror to humanity.</p>
+      </div>
+    </section>
+
+  </main>
+
+</body>
+
+</html>
